f6e73b3a7d7c75d7476294875df3420e9857c608
Starkgangs <Starkgangz@gmail.com>
2020-11-12 08:08:54 +0000

    pylint: auto fixes

--- fridaybot/modules/broadcast.py
+++ fridaybot/modules/broadcast.py
@@ -1,11 +1,14 @@
 import io
+
 from telethon.utils import pack_bot_file_id
+
+from fridaybot.Configs import Config
 from fridaybot.modules.sql_helper.broadcast_sql import (
     already_added,
     get_all_chnnl,
     rm_channel,
 )
-from fridaybot.Configs import Config
+
 
 @friday.on(friday_on_cmd(pattern="badd ?(.*)"))
 async def _(event):
